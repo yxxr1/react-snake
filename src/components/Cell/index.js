0c149e305847c7ab6dccd65142b04710ead06bff
YR <idUnd3fin3d@proton.me>
2024-06-22 00:19:44 +0300

    feat: snake shape

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -5,16 +5,27 @@ import styles from './styles.module.scss';
 export const STATES = {
   EMPTY: 1,
   SNAKE_HEAD: 2,
-  SNAKE_BODY: 3,
-  TARGET: 4,
-}
+  SNAKE_BODY_HORIZONTAL: 3,
+  SNAKE_BODY_VERTICAL: 4,
+  SNAKE_BODY_LEFT_TOP: 5,
+  SNAKE_BODY_RIGHT_TOP: 6,
+  SNAKE_BODY_LEFT_BOTTOM: 7,
+  SNAKE_BODY_RIGHT_BOTTOM: 8,
+  TARGET: 9,
+};
 
 export const Cell = memo(({ state }) => (
   <div className={styles.cell}>
     <div className={cx(styles.content, {
-      [styles.body]: state === STATES.SNAKE_BODY,
+      [styles.body]: state >= STATES.SNAKE_BODY_HORIZONTAL && state <= STATES.SNAKE_BODY_RIGHT_BOTTOM,
       [styles.head]: state === STATES.SNAKE_HEAD,
       [styles.target]: state === STATES.TARGET,
+      [styles['body-horizontal']]: state === STATES.SNAKE_BODY_HORIZONTAL,
+      [styles['body-vertical']]: state === STATES.SNAKE_BODY_VERTICAL,
+      [styles['body-left-top']]: state === STATES.SNAKE_BODY_LEFT_TOP,
+      [styles['body-right-top']]: state === STATES.SNAKE_BODY_RIGHT_TOP,
+      [styles['body-left-bottom']]: state === STATES.SNAKE_BODY_LEFT_BOTTOM,
+      [styles['body-right-bottom']]: state === STATES.SNAKE_BODY_RIGHT_BOTTOM,
     })} />
   </div>
 ));
--- src/components/Game/utils.js
+++ src/components/Game/utils.js
@@ -1,6 +1,23 @@
 import { STATES } from '../Cell';
 
 export const getBoard = (rows, cols, snake, target) => {
+  const isLeftCell = (leftCell, compareCell) => (
+    ((leftCell[1] + 1) % cols) === compareCell[1]
+  );
+  const isRightCell = (rightCell, compareCell) => {
+    const prev = rightCell[1] - 1;
+
+    return (prev < 0 ? prev + cols : prev) === compareCell[1]
+  };
+  const isTopCell = (topCell, compareCell) => (
+    ((topCell[0] + 1) % rows) === compareCell[0]
+  );
+  const isBottomCell = (bottomCell, compareCell) => {
+    const prev = bottomCell[0] - 1;
+
+    return (prev < 0 ? prev + rows : prev) === compareCell[0];
+  };
+
   const board = [];
 
   for (let row = 0; row < rows; row++) {
@@ -13,8 +30,33 @@ export const getBoard = (rows, cols, snake, target) => {
 
   board[snake[0][0]][snake[0][1]] = STATES.SNAKE_HEAD;
 
-  for (let i = 1; i < snake.length; i++) {
-    board[snake[i][0]][snake[i][1]] = STATES.SNAKE_BODY;
+  for (let i = 1; i < snake.length - 1; i++) {
+    const currentCell = snake[i];
+    const prevCell = snake[i - 1];
+    const nextCell = snake[i + 1];
+    let cellState;
+
+    if (prevCell[0] === currentCell[0] && nextCell[0] === currentCell[0]) {
+      cellState = STATES.SNAKE_BODY_HORIZONTAL;
+    } else if (prevCell[1] === currentCell[1] && nextCell[1] === currentCell[1]) {
+      cellState = STATES.SNAKE_BODY_VERTICAL;
+    } else if ((isLeftCell(prevCell, currentCell) && isTopCell(nextCell, currentCell)) || (isLeftCell(nextCell, currentCell) && isTopCell(prevCell, currentCell))) {
+      cellState = STATES.SNAKE_BODY_LEFT_TOP;
+    } else if ((isRightCell(prevCell, currentCell) && isTopCell(nextCell, currentCell)) || (isRightCell(nextCell, currentCell) && isTopCell(prevCell, currentCell))) {
+      cellState = STATES.SNAKE_BODY_RIGHT_TOP;
+    } else if ((isLeftCell(prevCell, currentCell) && isBottomCell(nextCell, currentCell)) || (isLeftCell(nextCell, currentCell) && isBottomCell(prevCell, currentCell))) {
+      cellState = STATES.SNAKE_BODY_LEFT_BOTTOM;
+    } if ((isRightCell(prevCell, currentCell) && isBottomCell(nextCell, currentCell)) || (isRightCell(nextCell, currentCell) && isBottomCell(prevCell, currentCell))) {
+      cellState = STATES.SNAKE_BODY_RIGHT_BOTTOM;
+    }
+
+    board[currentCell[0]][currentCell[1]] = cellState;
+  }
+
+  if (snake.length > 1) {
+    const lastCell = snake[snake.length - 1];
+    const prevLastCell = snake[snake.length - 2];
+    board[lastCell[0]][lastCell[1]] = lastCell[0] === prevLastCell[0] ? STATES.SNAKE_BODY_HORIZONTAL : STATES.SNAKE_BODY_VERTICAL;
   }
 
   if (target) {
